166a1d7430d3726a37d889270bfc0e323e134dea
Ashwin Vaz <mita.umalkar@skarma.com>
2016-02-09 18:13:02 +0530

    changes in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ $("#main").append('MITA, ');
+
+ var awesomethoughts = "I am Mita and I am awesome";
+ 
+ var funthoughts = awesomethoughts.replace ("awesome", "fun");
+
+ console.log(awesomethoughts);
+ console.log(funthoughts);
+
+ $('#main').append(funthoughts);
+
+ var name = "Mita Umalkar";
+ var role = "Web Developer";
+
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
